bf8f553e582f26aeb3aec93c6b3c087fa3930dae
Sewmini-Bhagya <sewminibhagya000@gmail.com>
2025-03-06 16:09:43 +0530

    Added Substraction.java to implement substraction

---
+++ Desktop/SimpleCalculator/Substraction.java
@@ -0,0 +1,11 @@
+public class Substraction {
+    	public static void main(String[] args) {
+        	int num1 = 5, num2 = 10;
+        	System.out.println("Difference: " + (num2 - num1));
+    	}
+}
+
+
+
+
+
